ec3405d6148702db45a1639d5c52b527760ba283
Juselania Silva <juselania98@outlook.com>
2021-02-09 11:58:05 -0300

    Implementação do Switch

--- logica/decisao/idades.dart
+++ logica/decisao/idades.dart
@@ -5,7 +5,7 @@
 // a partir de 65 vc é um idoso;
 
 main() {
-  var faixaEtaria = calcIdade(12);
+  var faixaEtaria = calcIdade(90);
   var faixaString = faixaEtariaToString(faixaEtaria);
 
   //print(faixaEtaria);
@@ -28,18 +28,26 @@ FaixaEtaria calcIdade(int idade) {
 
 enum FaixaEtaria { bebe, crianca, adolescente, adulto, idoso }
 
+// mudar o metodo faixaetariatostring para utilizar switch ao invez de ifs encadeados;
 String faixaEtariaToString(FaixaEtaria faixa) {
-  if (faixa == FaixaEtaria.bebe) {
-    return "Você é um bebe";
-  } else if (faixa == FaixaEtaria.crianca) {
-    return "Você é uma criança";
-  } else if (faixa == FaixaEtaria.adolescente) {
-    return "Você é um adolescente";
-  } else if (faixa == FaixaEtaria.adulto) {
-    return "Você é adulto";
-  } else {
-    return "Você é um idoso";
+  switch (faixa) {
+    case FaixaEtaria.bebe:
+      return "Você é um bebe";
+
+    case FaixaEtaria.crianca:
+      return "Você é uma criança";
+
+    case FaixaEtaria.adolescente:
+      return "Você é um adolescente";
+
+    case FaixaEtaria.adulto:
+      return "Você é adulto";
+
+    case FaixaEtaria.idoso:
+      return "Você é um idoso";
+      break;
+
+    default:
+      return "Idade Avançada!";
   }
 }
-
-// mudar o metodo faixaetariatostring para utilizar switch ao invez de ifs encadeados;
