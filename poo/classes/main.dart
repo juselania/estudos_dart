0ef617f2a78ae15dd137f200e6980bc548d78fed
Juselania Silva <juselania98@outlook.com>
2021-02-10 20:46:47 -0300

    Abstração

--- poo/classes/main.dart
+++ poo/classes/main.dart
@@ -1,8 +1,7 @@
 import 'cachorro.dart';
 
 main() {
-  var cachorro =
-      Cachorro(nome: 'Cacau', idade: 1, sexo: 'femnino');
+  var cachorro = Cachorro(nome: 'Cacau', idade: 1, sexo: 'femnino');
   print(
       "O animal ${cachorro.nome} tem ${cachorro.idade} anos e pertence ao sexo ${cachorro.sexo}");
 }
